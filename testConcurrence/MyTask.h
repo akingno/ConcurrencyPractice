09b280f3500aefd30b9433a6f3f2fd45c76c769e
akingno <aqingno@gmail.com>
2024-07-10 15:22:06 +0800

    dev:add totalSum but with errors

--- testConcurrence/MyTask.h
+++ testConcurrence/MyTask.h
@@ -4,24 +4,27 @@
 
 #ifndef TESTCONCURRENCE__MYTASK_H_
 #define TESTCONCURRENCE__MYTASK_H_
-#include <iostream>
+#include <atomic>
 #include <functional>
+#include <iostream>
 
-using TaskFunction = std::function<int(std::pair<int, int>)>;
+using TaskFunction = std::function<int(std::pair<int, int>,std::atomic<int>&)>;
 using Range = std::pair<int,int>;
 
 class Task{
   TaskFunction taskfunction;
   std::pair<int,int> m_range;
   int i_priority;
+  std::atomic<int>& m_totalSum;
  public:
   Task() = default;
-  Task(TaskFunction &&taskfun,Range &&range,int priority) : taskfunction(std::move(taskfun)),
-                                                              m_range(range),
-                                                              i_priority(priority){}
+  Task(TaskFunction taskfun,Range &&range,int priority, std::atomic<int>& totalSum) :taskfunction(taskfun),
+                                                                                        m_range(range),
+                                                                                        i_priority(priority),
+                                                                                        m_totalSum(totalSum){}
 
   auto execute(){
-    return taskfunction(m_range);
+    return taskfunction(m_range,m_totalSum);
   }
   bool operator<(const Task& other) const{
     return i_priority < other.i_priority;
--- testConcurrence/ThreadPool.h
+++ testConcurrence/ThreadPool.h
@@ -14,47 +14,27 @@
 
 class ThreadPool : std::enable_shared_from_this<ThreadPool>{
 
-  class ThreadStruct{
-    std::thread                 m_thread;
-    std::atomic<bool>           b_isInLeisure;
-    std::weak_ptr<ThreadPool>   m_threadPool;
-    std::mutex                  mtx_conditionMutex;
-    std::condition_variable     con_Var;//wait
-    friend class ThreadPool;
-   public:
-    explicit ThreadStruct(std::shared_ptr<ThreadPool>);//todo:此处好像无法用引用因为shared_from_this()
-    ~ThreadStruct();
-    void Run();
-    void Stop();
-    void Notify();
-  };
 
-  std::vector<std::shared_ptr<ThreadStruct>>  m_threads;
+  std::vector<std::thread>                    m_threads;
+  std::condition_variable                     con_Var;//wait
+
+
   std::priority_queue<Task>                   pq_taskPriorityQueue;
   std::mutex                                  mtx_queueMutex;
-  std::condition_variable                     con_condition;
 
   bool                                        b_stopFlag;
-
-
   void Init(int);
-  bool IsFull() const;
- public:
+  void Run();
+  void Stop();
+
+  public:
   explicit ThreadPool(int);
   //ThreadPool(ThreadPool&);
   ~ThreadPool();
   //void operator=(ThreadPool);
   //void operator=(ThreadPool&);
-  void AddingThreads(int);
-  void NotifiedByTaskqueue();
-  void DispatchTask();
-  void Stop();
   void EnqueueTask(Task&&);
 
-
-
-
-
-
+  bool IsFull() const;
 };
 #endif//TESTCONCURRENCE__THREADPOOL_H_
